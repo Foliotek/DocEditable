dcc9d3a84595bf445432474c0655f2e715cd846a
bgrins <briangrinstead@gmail.com>
2013-03-19 14:55:49 -0500

    Stub out methods in docs

--- test/tests.js
+++ test/tests.js
@@ -32,36 +32,19 @@ test("Basic Formatting", function() {
   wysi.bold({ line: 0, ch: 1 }, { line: 0, ch: 7 });
   equal (wysi.toHTML(), "<strong>T</strong><strong>esting</strong><br />formatting", "Calling bold again should mark text again");
 
-  /*
-  wysi.setValue("Testing\nformatting");
-
-  wysi.bold({ line: 0, ch: 0 }, { line: 0, ch: 7 });
-  equal (wysi.toHTML(), "<strong>Testing</strong><br />formatting", "Calling bold should mark text");
-
-  wysi.bold({ line: 0, ch: 2 }, { line: 0, ch: 7 });
-  equal (wysi.toHTML(), "<strong>Te</strong>sting<br />formatting", "Calling bold again should remove formatting");
-
-  wysi.bold({ line: 0, ch: 2 }, { line: 0, ch: 3 });
-  equal (wysi.toHTML(), "<strong>Te</strong>s<strong>ting</strong><br />formatting", "Calling bold again should remove formatting");
-
-  wysi.bold({ line: 0, ch: 2 }, { line: 0, ch: 7 });
-  equal (wysi.toHTML(), "Testing<br />formatting", "Calling bold again should re-add formatting");
-  */
-
+  
   wysi.setValue("Testing\nformatting");
 
   wysi.bold({ line: 0, ch: 1 }, { line: 1, ch: 2 });
-  equal (wysi.toHTML(), "T<strong>esting<br />fo</strong>rmatting", "Calling bold should span lines");
+  equal (wysi.toHTML(), "T<strong>esting</strong><br /><strong>fo</strong>rmatting", "Calling bold should split lines");
 
 
   wysi.setValue("Testing\nformatting");
 
   wysi.italic({ line: 0, ch: 0 }, { line: 1, ch: 2 });
-  equal (wysi.toHTML(), "<em>Testing<br />fo</em>rmatting");
+  equal (wysi.toHTML(), "<em>Testing</em><br /><em>fo</em>rmatting", "Calling italic should split lines");
 
   wysi.italic({ line: 0, ch: 1 }, { line: 0, ch: 3 });
-  equal (wysi.toHTML(), "<em>T</em>es<em>ting<br /></em>formatting");
-
-
+  equal (wysi.toHTML(), "<em>T</em>es<em>ting</em><br /><em>fo</em>rmatting", "Calling italic again should split selection");
 
 });
